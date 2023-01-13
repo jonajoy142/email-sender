9201732b6b9f2fa8a8d0e896cf273a3a0da99f85
Jona Joy <jonajoy142@gmail.com>
2023-01-13 19:11:58 +0530

    Transport Created!

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+const { text } = require('express');
 const express  = require('express')
 const nodemailer = require('nodemailer')
 const app = express();
 const port = 5000
 
+function sendMail(){
 
+    return new Promise((resolve,reject)=>{
+        var transporter = nodemailer.createTransport({
+            service:'gmail',
+            auth:{
+                user:'',
+                pass:''
+            }
+        })
+        const mail_configs = {
+            from:'',
+            to:'',
+            subject:'Testing code for sending mail!',
+            text:"Hai peter,This is a Sample mail to test coding!."
+        }
+        
+    })
+}
 
 app.listen(port,()=>{
     console.log(`Listeneing to port: ${port}`)
